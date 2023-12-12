6dc7db185117ef14d94361fb8ded017a714cd90c
Anastasia <grey-mice@mail.ru>
2023-12-12 20:59:46 +0300

    Revert "Добавлена функция, которая делает буквы с заглавными  и написала докстринги к обеим функциям"
    This reverts commit 78daf35bb81a1942672adce89d02d2bc6661b3ef.

--- str_func.py
+++ str_func.py
@@ -1,7 +1,2 @@
 def go_upper(str_word):
-    """делает бум бум со словом"""
     return str_word.upper()
-
-def do_title(text):
-    """делает каждое слово строки с большой буквы"""
-    return text.title()
