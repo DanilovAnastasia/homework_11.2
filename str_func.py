78daf35bb81a1942672adce89d02d2bc6661b3ef
Anastasia <grey-mice@mail.ru>
2023-12-12 20:31:27 +0300

    Добавлена функция, которая делает буквы с заглавными  и написала докстринги к обеим функциям

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def go_upper(str_word):
+    """делает бум бум со словом"""
     return str_word.upper()
+
+def do_title(text):
+    """делает каждое слово строки с большой буквы"""
+    return text.title()
