a6513612aa708cbe696debcf4721918c8cf54c37
Anastasia <grey-mice@mail.ru>
2023-12-12 21:05:47 +0300

    Добавлены докстринги к двум функциям. Добавлена функция, которая делает буквы с заглавными.

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def go_upper(str_word):
+    """добавлен чух чух"""
     return str_word.upper()
+
+
+def do_title(text):
+    """добавлена функция, которая делает текст с заглавными буквами"""
+    return text.title()
